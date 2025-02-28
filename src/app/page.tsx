2b398dd4272cb9f9f5eee713961869f28f4d7a37
Ashutosh :) <bro4code@gmail.com>
2025-02-28 20:57:49 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <h1>Trying nextjs </h1>
+    <div className="flex flex-col items-center justify-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+     <span>Website is currently in build ⚡</span>
+     <span>Contant: "bro4code@gmail.com" for any quieries</span>
     </div>
   );
 }
